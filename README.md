459a62babbca93644fe0df318244d53dd04601e5
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-24 11:07:55 +0000

    Create initial package

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RectAngle
 Segmentation and classification tool for trans-rectal B-mode ultrasound images.
+
+*Submitted as coursework for MPHY0041: Machine Learning in Medical Imaging.*
+
+This package contains PyTorch-based implementations of a U-Net based segmentation model, and a **TBC** based classification model, for the simultaneous detection and segmentation of prostate in rectal b-mode ultrasound images.
+
+**NEED TO WRITE UP REST OF DETAILS**
+
+## Installation
+A conda environment file is provided in the ./conda/ folder, which may be used as such:
+
+>conda create env --file ./conda/rectangle.yml
+>conda activate rectangle
+
+Once this is activated, the package may be installed using the *setup.py* file:
+
+>python ./setup.py
+
+Following this, training/inference may be performed from the command line using the scripts *train.py* and *predict.py* respectively.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RectAngle", # Replace with your own username
+    version="0.0.1",
+    author="Liam Chalcroft",
+    author_email="liam.chalcroft.20@ucl.ac.uk",
+    description="Ultrasound prostate image analysis using CNNs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liamchalcroft/RectAngle",
+    project_urls={
+        "Bug Tracker": "https://github.com/liamchalcroft/RectAngle/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "RectAngle"},
+    packages=setuptools.find_packages(where="RectAngle"),
+    python_requires=">=3.6",
+)
