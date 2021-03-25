77c4f8333a9ca651b2ed30945421d8989b4ce2ba
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-25 17:28:36 +0000

    Remove annoying input on data.py

--- data.py
+++ data.py
@@ -29,20 +29,20 @@ print('  with {:d} cases with a total of {:d} frames and {:d} labels.'.format(
     len([k for k in h5_file.keys() if k.split('_')[0]=='label'])))
 
 
-print('\nHow many example frames would you like to visualise, e.g. 10?')
-num_case_plots = int(input())
+# print('\nHow many example frames would you like to visualise, e.g. 10?')
+# num_case_plots = int(input())
 
-## plot a few examples
-for _ in range(num_case_plots):
-    case_id = random.randint(0,num_cases-1) # draw a random case
+# ## plot a few examples
+# for _ in range(num_case_plots):
+#     case_id = random.randint(0,num_cases-1) # draw a random case
 
-    frame_key = random.choice([k for k in h5_file.keys() if (k.split('_')[0]=='frame' and k.split('_')[1]=='%04d'%case_id)])
-    label_keys = [k for k in h5_file.keys() if (k.split('_')[0]=='label' and frame_key.replace('frame','label') in k)]
+#     frame_key = random.choice([k for k in h5_file.keys() if (k.split('_')[0]=='frame' and k.split('_')[1]=='%04d'%case_id)])
+#     label_keys = [k for k in h5_file.keys() if (k.split('_')[0]=='label' and frame_key.replace('frame','label') in k)]
 
-    montage = np.concatenate([h5_file[frame_key]] + [h5_file[k]*np.max(h5_file[frame_key]) for k in label_keys],axis=0)
-    plt.figure()
-    plt.imshow(montage, cmap='gray')
-    plt.axis('off')
-    plt.savefig('case{:04d}'.format(case_id)+'.jpg',bbox_inches='tight')
-    # plt.show()
-    # plt.close()
+#     montage = np.concatenate([h5_file[frame_key]] + [h5_file[k]*np.max(h5_file[frame_key]) for k in label_keys],axis=0)
+#     plt.figure()
+#     plt.imshow(montage, cmap='gray')
+#     plt.axis('off')
+#     plt.savefig('case{:04d}'.format(case_id)+'.jpg',bbox_inches='tight')
+#     # plt.show()
+#     # plt.close()
