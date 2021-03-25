d8f3f510e5c567b8b7a5e22d2dc5290ab492ec15
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-25 09:34:09 +0000

    Lower case package for imports

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "RectAngle"},
-    packages=[setuptools.find_packages(where="RectAngle"),
+    package_dir={"": "rectangle"},
+    packages=[setuptools.find_packages(where="rectangle"),
     python_requires=">=3.6",
 )
