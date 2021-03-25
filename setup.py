e07fe2c5ba54e6277671f4d6e5f25e690b89a88a
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-25 09:41:52 +0000

    Undo lower case, rename to src

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "rectangle"},
-    packages=[setuptools.find_packages(where="rectangle"),
+    package_dir={"": "src"},
+    packages=[setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
