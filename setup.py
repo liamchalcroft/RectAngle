d49ab364b65dfb94c484b1f0f79e513acd9dcd6b
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-03-25 10:04:24 +0000

    More missing brackets

--- setup.py
+++ setup.py
@@ -21,4 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=[setuptools.find_packages(where="src"))
+    packages=[setuptools.find_packages(where="src")],
+    python_requires=">=3.6",
+)
