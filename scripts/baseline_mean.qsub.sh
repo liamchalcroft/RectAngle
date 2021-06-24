3326aa8b1fead8fb297d3025f01e6fea647a90b9
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2021-06-24 13:25:40 +0100

    Add qsub scripts

---
+++ scripts/baseline_mean.qsub.sh
@@ -0,0 +1,23 @@
+#$ -S /bin/bash
+#$ -l tmem=32G
+#$ -l h_vmem=32G
+#$ -l h_rt=40:00:00
+
+#$ -l gpu=true
+#$ -N baseline
+
+#$ -cwd
+
+#module purge
+#module load default/python/3.8.5
+source /share/apps/source_files/python/python-3.8.5.source
+source rectenv/bin/activate
+
+python ./RectAngle/train.py --train ./miccai_us_data/train.h5 \
+--val ./miccai_us_data/val.h5 \
+--ensemble 5 \
+--lr_schedule exponential \
+--label mean \
+--odir ./baseline_data/mean \
+--epochs 50 \
+--seed 0
